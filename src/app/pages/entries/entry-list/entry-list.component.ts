63bbe2b2fada94b5f11a08b681d02d78f989bfa9
Wanderson Xesquevixos de Siqueira <wandersonxs@gmail.com>
2019-04-15 13:24:19 -0300

    first commit

---
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {CategoryService} from '../shared/category.service';
+import {Category} from '../shared/category.model';
+
+@Component({
+  selector: 'app-category-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.css']
+})
+export class CategoryListComponent implements OnInit {
+
+  categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService) { }
+
+  ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert('Erro ao carregar a lista')
+    );
+  }
+
+  deleteCategory(category) {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+    if ( mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(element => element !== category)
+      );
+    }
+  }
+
+
+}
