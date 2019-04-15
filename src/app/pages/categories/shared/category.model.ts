195a283c1699a4e62d3e9f352c6f79ddfa442807
Wanderson Xesquevixos de Siqueira <wandersonxs@gmail.com>
2019-04-15 14:06:45 -0300

    adding BaseResourceModel

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,8 +1,12 @@
-export class Category {
+import {BaseResourceModel} from '../../../shared/models/base-resource.model';
+
+export class Category extends BaseResourceModel {
   constructor(
     public id?: number,
     public name?: string,
     public description?: string
-  ){}
+  ) {
+    super();
+  }
 
 }
