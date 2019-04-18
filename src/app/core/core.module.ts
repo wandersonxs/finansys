ff2b30d079fd932f4fcc2e2f961c823682771295
Wanderson Xesquevixos de Siqueira <wandersonxs@gmail.com>
2019-04-18 08:47:54 -0300

    Changes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,22 +5,28 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
 import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {InMemoryDatabase} from '../in-memory-database';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+    RouterModule
   ],
   exports:
     [
       // shared modules
       BrowserModule,
       BrowserAnimationsModule,
-      HttpClientModule
+      HttpClientModule,
+      // Shared Components
+      NavbarComponent,
+      RouterModule
     ]
 })
 export class CoreModule {
